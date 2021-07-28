f86538a1adab915cce7209d5f9bdfc4fe368b6c0
kcsheng <kacysheng@mail.com>
2021-07-28 15:47:52 +1000

    Update readme on development plan

---
+++ src/getInput.js
@@ -0,0 +1,15 @@
+// const inquirer = require("inquirer");
+// const nameValidation = (input) => {};
+// const questions = [
+//   {
+//     type: "input",
+//     name: "manager_name",
+//     message: "Enter manager's name:",
+//     validate: (input) => {
+//       const regexp = new RegExp(/^[a-z ,.'-]+$/, "i");
+//       return regexp.test(input) && input.length > 0 ? true : "Name invalid!";
+//     },
+//   },
+// ];
+
+// inquirer.prompt(questions).then((data) => console.log(data));
