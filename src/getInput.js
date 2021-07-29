56222f266b823435bcc888497ab2c01e7088226c
kcsheng <kacysheng@mail.com>
2021-07-29 11:48:59 +1000

    Finish question preparation for inquirer

--- src/getInput.js
+++ src/getInput.js
@@ -1,15 +1,31 @@
-// const inquirer = require("inquirer");
-// const nameValidation = (input) => {};
-// const questions = [
-//   {
-//     type: "input",
-//     name: "manager_name",
-//     message: "Enter manager's name:",
-//     validate: (input) => {
-//       const regexp = new RegExp(/^[a-z ,.'-]+$/, "i");
-//       return regexp.test(input) && input.length > 0 ? true : "Name invalid!";
-//     },
-//   },
-// ];
+const Employee = require("../lib/employee");
+const inquirer = require("inquirer");
 
-// inquirer.prompt(questions).then((data) => console.log(data));
+const askQuestionsBy = (position) => {
+  const questions = [];
+  const keys = [`${position}_name`, `${position}_id`, `${position}_email`];
+  const extraKeys = {
+    manager: `${position}_office_ext`,
+    engineer: `${position}_github`,
+    intern: `${position}_school`,
+  };
+  keys.push(extraKeys[position]);
+
+  for (let i = 0; i < keys.length; i++) {
+    questions.push({
+      type: "input",
+      name: `${keys[i]}`,
+      message: `${Employee.questions(position)[i]}`,
+      validate: (input) => {
+        const regexp = new RegExp(/^[0-9 a-z,.'-@]+$/, "i");
+        return regexp.test(input) && input.length > 0
+          ? true
+          : "Input not valid!";
+      },
+    });
+  }
+  return questions;
+};
+
+let questions = askQuestionsBy("engineer");
+inquirer.prompt(questions).then((data) => console.log(data));
