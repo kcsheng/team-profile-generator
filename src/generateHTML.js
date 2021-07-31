5551a0652dd8e296a90f390e4bbf0e8e2c8015d3
kcsheng <kacysheng@mail.com>
2021-07-31 22:42:20 +1000

    Edit font awesome link

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -44,9 +44,12 @@ const writeHTML = (team) => {
       <link rel="preconnect" href="https://fonts.googleapis.com">
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
       <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-      <script src="https://kit.fontawesome.com/3b6c858269.js" crossorigin="anonymous"></script>
       <link rel="stylesheet" href="./assets/css/reset.css">
       <link rel="stylesheet" href="./assets/css/style.css">
+      <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+      integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="crossorigin="anonymous" referrerpolicy="no-referrer"/>
       <title>Team Profile</title>
     </head>
     <body>
