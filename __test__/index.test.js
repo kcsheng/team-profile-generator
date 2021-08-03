ed776edf4f5d2b91ff677dc7a9faa805ba594525
kcsheng <kacysheng@mail.com>
2021-08-04 01:33:12 +1000

    Inquirer mocking awaits modification

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,9 +1,6 @@
 const { addEmployee, askQuestionsBy, createTeam } = require("../index");
 const inquirer = require("inquirer");
 
-// Mock the inquirer module
-// jest.mock("inquirer");
-
 describe("index", () => {
   describe("askQuestionsBy(position)", () => {
     it("should match corresponding property value for a specific position", () => {
@@ -17,10 +14,26 @@ describe("index", () => {
     });
   });
 
-  // describe("addEmployee", () => {
-  //   it("should return empty object by invoking addEmployee with 'manager'", () => {
-  //     inquirer.prompt.mockResolvedValue({ data: {} });
-  //     expect(addEmployee("manager")).resolves.toEqual({ data: {} });
+  // describe("inquirer", () => {
+  //   it("should be invoked by askQuestionsby('manager')", () => {
+  //     inquirer.prompt = jest.fn().mockImplementation(() =>
+  //       Promise.resolve({
+  //         manager_name: "John Jackson",
+  //         manager_id: "31454",
+  //         manager_email: "jj@cute.me",
+  //         manager_office_ext: "3451",
+  //         further_action: "Finish team building.",
+  //         position: "manager",
+  //       })
+  //     );
+  //     expect(addEmployee("manager")).resolves.toEqual({
+  //       manager_name: "John Jackson",
+  //       manager_id: "31454",
+  //       manager_email: "jj@cute.me",
+  //       manager_office_ext: "3451",
+  //       further_action: "Finish team building.",
+  //       position: "manager",
+  //     });
   //   });
   // });
 
